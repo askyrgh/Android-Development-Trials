0786208f1041062f095820f79391eef7e305f6de
SKY <akash.rm.roy@gmail.com>
2021-03-28 22:29:57 +0530

    Splash Screen tutorial completed

---
+++ Miscellaneous/SplashScreen/app/src/main/java/com/askyr/splashscreen/MainActivity.java
@@ -0,0 +1,15 @@
+package com.askyr.splashscreen;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setTheme(R.style.Theme_SplashScreen);
+        setContentView(R.layout.activity_main);
+    }
+}
