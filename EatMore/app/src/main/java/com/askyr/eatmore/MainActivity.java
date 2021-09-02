cb54d9e6289fe058abb59f42e1593de7ba76beb0
SKY-ROY <akash.rm.roy@gmail.com>
2021-09-03 00:31:53 +0530

    MainActivity layout completed and imported all design assets

--- EatMore/app/src/main/java/com/askyr/eatmore/MainActivity.java
+++ EatMore/app/src/main/java/com/askyr/eatmore/MainActivity.java
@@ -1,15 +1,28 @@
 package com.askyr.eatmore;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.constraintlayout.widget.ConstraintLayout;
 
 import android.os.Bundle;
+import android.view.View;
 
 public class MainActivity extends AppCompatActivity {
 
+    private ConstraintLayout cl_welcome;
+    
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setTheme(R.style.Theme_EatMore);
         setContentView(R.layout.activity_main);
+
+        cl_welcome = findViewById(R.id.cl_welcome);
+
+        cl_welcome.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                cl_welcome.setVisibility(View.GONE);
+            }
+        });
     }
 }
