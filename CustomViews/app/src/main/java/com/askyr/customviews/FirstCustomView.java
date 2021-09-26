a84a71dbbf760f6a1c45c9bcbd19d37c8e6067dd
SKY-ROY <akash.rm.roy@gmail.com>
2021-09-26 23:57:38 +0530

    FirstCustomView layout and layout inflation implemented

--- CustomViews/app/src/main/java/com/askyr/customviews/FirstCustomView.java
+++ CustomViews/app/src/main/java/com/askyr/customviews/FirstCustomView.java
@@ -1,4 +1,57 @@
 package com.askyr.customviews;
 
-public class FirstCustomView {
+import android.content.Context;
+import android.util.AttributeSet;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.widget.Button;
+import android.widget.FrameLayout;
+import android.widget.Toast;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+
+public class FirstCustomView extends FrameLayout {
+
+    private Button button1;
+    private OnClickListener button1onClickListener;
+
+    // Constructor for attribute-less initialization through Java
+
+    public FirstCustomView(@NonNull Context context) {
+        super(context);
+        init();
+    }
+
+    // Constructors for attribute based initialization through XML
+
+    public FirstCustomView(@NonNull Context context, @Nullable AttributeSet attrs) {
+        super(context, attrs);
+        init();
+    }
+
+    public FirstCustomView(@NonNull Context context, @Nullable AttributeSet attrs, int defStyleAttr) {
+        super(context, attrs, defStyleAttr);
+        init();
+    }
+
+    // This method will be called for initializing, everytime we use this view.
+
+    private void init() {
+        View view = LayoutInflater.from(getContext()).inflate(R.layout.first_custom_view_layout, this);
+        button1 = findViewById(R.id.btn_button1);
+
+        button1.setOnClickListener(new OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(getContext(), "Clicked Button-1", Toast.LENGTH_SHORT).show();
+            }
+        });
+    }
+
+    // Dynamically binding the on-click listener of the button externally
+
+    public void setButton1onClickListener(OnClickListener button1onClickListener) {
+        this.button1onClickListener = button1onClickListener;
+    }
 }
--- CustomViews/app/src/main/java/com/askyr/customviews/MainActivity.java
+++ CustomViews/app/src/main/java/com/askyr/customviews/MainActivity.java
@@ -3,12 +3,32 @@ package com.askyr.customviews;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
+
+    private FirstCustomView customView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        customView = findViewById(R.id.exm1_FirstCustomView);
+
+        customView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Toast.makeText(MainActivity.this, "Clicked FirstCustomView exm1", Toast.LENGTH_SHORT).show();
+            }
+        });
+
+        customView.setButton1onClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                
+            }
+        });
     }
 }
