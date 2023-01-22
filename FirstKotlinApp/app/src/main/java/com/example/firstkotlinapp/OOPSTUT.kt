bf3ffb0b8522732523f96da1d932023639c1f9e3
akash_roy <akash.rm.roy@gmail.com>
2023-01-23 01:01:34 +0530

    Update OOPSTUT.kt

--- FirstKotlinApp/app/src/main/java/com/example/firstkotlinapp/OOPSTUT.kt
+++ FirstKotlinApp/app/src/main/java/com/example/firstkotlinapp/OOPSTUT.kt
@@ -1,58 +1,98 @@
 package com.example.firstkotlinapp
 
+import java.lang.IllegalArgumentException
+
 fun main() {
-    var sky: Person = Person("SKY", "ROY")
-    var def: Person = Person()
+    scopeAndShadowingDemo(9)
 
-    myFunction(9)
+    constructorDemo()
 
-    sky.stateHobby()
-    sky.hobby = "gaming"
-    sky.stateHobby()
+    lateinitDemo()
 
-    var k: Key = Key()
-
+    DataClassDemo()
 }
 
-class Person constructor(firstName:String = "FirstName", lastName: String = "LastName"){
-    // Example of a class
-
-    // Member Variables - Properties
-    var age: Int? = null
-    var hobby: String = "painting"
-    var firstName: String? = null
-
-    // Member Functions - Methods
-    fun stateHobby(){
-        println("My hobby is $hobby")
-    }
-
-    // Initializer block
-    init {
-        println("Initialized a new Person with FN: ${firstName} & LN: ${lastName}")
-        this.firstName = firstName
-    }
-
-    // Member Constructor - Secondary constructor
-    constructor(firstName: String, lastName: String, age: Int) : this(firstName, lastName) {
-        // returning an object by use of default constructor
-        // via passing the parameters as arguments of default constructor
-        // after initializing the respective member variables by the parameters
-
-        this.age = age
-        print("Initialized a new Person with FN: $firstName LN: $lastName AGE: $age")
-    }
-}
-
-fun myFunction(a: Int) {
+fun scopeAndShadowingDemo(a: Int) {
     var a = 10
     println("a = $a")
 }
 
-class Key{
-    var keyId: Int? = null
-    
+fun constructorDemo() {
+    var sky: Person = Person("SKY", "ROY")
+    var def: Person = Person()
+}
+
+// Constructor example
+class Person constructor(firstName:String = "FirstName", lastName: String = "LastName"){
+    // Example of a class
+
+    // Initializer block
     init {
-        println("Key ID: $keyId")
+        println("Initialized a new Person with FN: ${firstName} & LN: ${lastName}")
     }
 }
+
+fun lateinitDemo() {
+    var myCar = Car()
+    println(myCar.owner)
+    println("brand is ${myCar.brand}")
+    myCar.topSpeed = 200f
+    println("max speed is ${myCar.topSpeed}")
+    println("model is ${myCar.model}")
+}
+
+// lateinit example
+class Car() {
+    lateinit var owner : String // doesn't need to be initialized immediately
+    val brand : String = "BMW"
+        // custom getter
+        get() {
+            return field.lowercase()
+        }
+
+    var topSpeed: Float = 250f
+        // field keyword is used to avoid direct use of variable which will lead to recursive calls causing stack overflow
+        get() = field
+        // custom setter
+        // value is the variable that stores the data passed as argument and used to assign to the respective field
+        set(value) {
+            field = if(value>0) value else throw IllegalArgumentException("Max Speed can't be less than zero")
+        }
+
+    var model: String = "Series 3 Gran Limousine"
+        private set
+
+    init {
+        owner = "SKY"
+        model = "3GL"
+    }
+}
+
+fun DataClassDemo() {
+    var user1 = User(1, "SKY")
+    val name = user1.name
+    println(name)
+    user1.name = "AKASH"
+
+    val user2 = User(2, "RAHUL")
+
+    println(user1 == user2)
+
+    // object copying
+    val updatedUser = user1.copy(name = "ASKYR")
+    println(updatedUser)
+
+    println(user1)
+
+    // component fetching
+    println(updatedUser.component1())   // prints 1
+    println(updatedUser.component2())   // prints ASKYR
+
+    // deconstruction
+    val (id_copy, name_copy) =  updatedUser
+    println("id=$id_copy")
+    println("name=$name_copy")
+}
+
+// Data Class example
+data class User(val id: Long, var name: String)
