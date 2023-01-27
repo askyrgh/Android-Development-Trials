44c43171d8c8d44e56275a6bcf0becb99e1ebb2e
akash_roy <akash.rm.roy@gmail.com>
2023-01-28 02:38:50 +0530

    Update OOPSTUT.kt

--- FirstKotlinApp/app/src/main/java/com/example/firstkotlinapp/OOPSTUT.kt
+++ FirstKotlinApp/app/src/main/java/com/example/firstkotlinapp/OOPSTUT.kt
@@ -10,6 +10,10 @@ fun main() {
     lateinitDemo()
 
     DataClassDemo()
+
+//    InheritanceDemo()
+
+    InterfaceDemo()
 }
 
 // Scope and shawdowing example
@@ -35,7 +39,7 @@ class Person constructor(firstName:String = "FirstName", lastName: String = "Las
 
 // lateinit example
 fun lateinitDemo() {
-    var myCar = Car()
+    var myCar = SimpleCar()
     println(myCar.owner)
     println("brand is ${myCar.brand}")
     myCar.topSpeed = 200f
@@ -43,10 +47,11 @@ fun lateinitDemo() {
     println("model is ${myCar.model}")
 }
 
-class Car() {
+class SimpleCar() {
     lateinit var owner : String // doesn't need to be initialized immediately
     val brand : String = "BMW"
         // custom getter
+
         get() {
             return field.lowercase()
         }
@@ -97,3 +102,185 @@ fun DataClassDemo() {
 }
 
 data class User(val id: Long, var name: String)
+
+//Inheritance Example
+
+open class Vehicle {
+    // properties
+    // methods
+}
+
+//open class Car(val name: String, val brand: String){
+//    // use of open keyword so the property can be overridden in derived class
+//    open var range: Double = 0.0
+//
+//    // use of open keyword so the method can be overridden in derived class
+//    open fun extendRange(amount: Double) {
+//        if(amount > 0) {
+//            range += amount
+//        }
+//    }
+//
+//    open fun drive(distance: Double) {
+//        println("Drove for ${distance} KMs")
+//    }
+//}
+//
+//class ElectricCar(name: String, brand: String, batteryLife: Double) : Car(name, brand) {
+//    var chargerType : String = "Type1"
+//    override var range = batteryLife * 6
+//
+//    override fun drive(distance: Double) {
+//        super.drive(distance)
+//        println("Drove ${distance} KMs on electricity")
+//    }
+//
+//    fun drive() {
+//        println("Drove ${range} KMs on electricity")
+//    }
+//}
+//
+//fun InheritanceDemo() {
+//    println()
+//    println("---Inheritance Demo---")
+
+//    var audiA3 = Car("A3", "Audi")
+//    var teslaS = ElectricCar("S-Model", "Tesla", 85.0)
+//
+//    teslaS.toString()   // this is possible because all classes in Kotlin inherit the 'Any' Class as super class by default
+//
+//    teslaS.extendRange(100.0)   // this is possible for ElectricCar object because it inherits QuadCar
+//
+//    teslaS.chargerType = "Type2"
+//
+//    // Polymorphism - Ability to treat objects with similar traits in a common way
+//    audiA3.drive(200.0)
+//    teslaS.drive(200.0)
+//}
+
+// Interface example
+interface Drivable {
+    val maxSpeed : Double
+    fun drive() : String
+    fun brake() {
+        println("The drivable is braking")
+    }
+}
+
+// Implementation of maxSpeed property in Drivable
+open class Car(override val maxSpeed : Double, val name: String, val brand: String) : Drivable{
+    // use of open keyword so the property can be overridden in derived class
+    open var range: Double = 0.0
+
+    // use of open keyword so the method can be overridden in derived class
+    open fun extendRange(amount: Double) {
+        if(amount > 0) {
+            range += amount
+        }
+    }
+
+    // This drive() method's signature was not same as the Drivable drive()
+    // therefore it isn't valid implementation of the former
+    open fun drive(distance: Double) {
+        println("Drove for ${distance} KMs")
+    }
+
+    // Implementation of drive() method in Drivable
+    override fun drive(): String {
+        return "driving the interface drive"
+    }
+}
+
+class ElectricCar(maxSpeed: Double, name: String, brand: String, batteryLife: Double) : Car(maxSpeed, name, brand) {
+    var chargerType : String = "Type1"
+    override var range = batteryLife * 6
+
+    override fun drive(distance: Double) {
+        super.drive(distance)
+        println("Drove ${distance} KMs on electricity")
+    }
+
+    override fun drive() : String {
+        return "Drove ${range} KMs on electricity"
+    }
+
+    override fun brake() {
+        super.brake()
+        println("Brake inside of electric car")
+    }
+}
+
+fun InterfaceDemo() {
+    println()
+    println("---Interface Demo---")
+
+    var audiA3 = Car(200.0,"A3","Audi")
+    var teslaS = ElectricCar(240.0,"S-Model","Tesla",85.0)
+
+    teslaS.toString()   // this is possible because all classes in Kotlin inherit the 'Any' Class as super class by default
+
+    teslaS.extendRange(100.0)   // this is possible for ElectricCar object because it inherits QuadCar
+
+    audiA3.drive(200.0)
+    teslaS.drive(200.0)
+
+    teslaS.drive()
+    teslaS.brake()
+
+    audiA3.drive()
+    audiA3.brake()
+}
+
+// Abstract Class example
+
+// Concrete non abstract properties - name, origin, weight
+abstract class Mammal(private val name: String, private val origin: String, private val weight: Double) {
+    // Abstract properties (Must be overridden by subclass)
+    abstract val maxSpeed: Double
+
+    // Abstract methods (Must be overridden by subclass)
+    abstract fun run()
+    abstract fun breath()
+
+    // Concrete Non-abstract methods
+    fun displayDetails() {
+        println(" Name: $name")
+        println(" Origin: $origin")
+        println(" Weight: $weight")
+        println(" Max Speed: $maxSpeed")
+    }
+}
+
+class Human(name: String, origin: String, weight: Double, override val maxSpeed: Double): Mammal(name, origin, weight) {
+    override fun run() {
+        println("Runs on 2 legs")
+    }
+
+    override fun breath() {
+        println("Breathes through mouth or nose")
+    }
+}
+
+class Elephant(name: String, origin: String, weight: Double, override val maxSpeed: Double): Mammal(name, origin, weight) {
+    override fun run() {
+        println("Runs on 4 legs")
+    }
+
+    override fun breath() {
+        println("Breathes through the trunk")
+    }
+}
+
+fun AbstractClassDemo() {
+    println()
+    println("---Abstract Class Demo---")
+
+    val human = Human("Akash", "India", 74.0, 3.6)
+    val elephant = Human("Rosy", "India", 5400.0, 11.2)
+
+    human.breath()
+    elephant.breath()
+
+    human.run()
+    elephant.run()
+}
