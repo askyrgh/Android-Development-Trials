0571db237098190acc16cd50358cc834c66c75fe
Akash <akash.rm.roy@gmail.com>
2025-06-16 00:20:25 +0530

    -Test case added for Success Path for Unscramble app

--- kotlin-compose/basic-android-kotlin-compose-training-unscramble/app/build.gradle.kts
+++ kotlin-compose/basic-android-kotlin-compose-training-unscramble/app/build.gradle.kts
@@ -67,6 +67,7 @@ android {
 
 dependencies {
     implementation(platform("androidx.compose:compose-bom:2025.04.00"))
+
     implementation("androidx.activity:activity-compose:1.10.1")
     implementation("androidx.compose.material3:material3")
     implementation("androidx.compose.ui:ui")
@@ -76,6 +77,9 @@ dependencies {
     implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.8.7")
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.7")
 
+    testImplementation("junit:junit:4.13.2")
+
     debugImplementation("androidx.compose.ui:ui-test-manifest")
     debugImplementation("androidx.compose.ui:ui-tooling")
+    testImplementation(kotlin("test"))
 }
